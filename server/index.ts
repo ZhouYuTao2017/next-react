86efb4f1507362d197d5791fc968f1ebac7e66a4
zyt <1619686166@qq.com>
2020-05-09 18:01:28 +0800

    feat: next server

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,6 @@ import http from 'http';
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const server = express()
 const handle = app.getRequestHandler()
 
 app.prepare()
